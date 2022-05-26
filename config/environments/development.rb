28d94911c706bbb7184f56ee5839cd60ac339305
rutiksimform <rutik.p@simformsolutions.com>
2022-05-26 13:46:21 +0530

    bug fixing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  # Configuration for letter opener
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
 end
